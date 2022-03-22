e7ad4825f5a40ddbab9e57ceda8a4a0c49ba58d2
Julynx <64993676+Julynx@users.noreply.github.com>
2022-03-22 19:51:06 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -16,8 +16,7 @@ cp -r bin $HOME/flatty/
 cp -r icons $HOME/flatty/
 
 # Generate a .desktop file
-echo "" > $HOME/.local/share/applications/flatty.desktop
-echo "[Desktop Entry]" >> $HOME/.local/share/applications/flatty.desktop
+echo "[Desktop Entry]" > $HOME/.local/share/applications/flatty.desktop
 echo "Name=Flatty" >> $HOME/.local/share/applications/flatty.desktop
 echo "Comment='Simple flatpak GUI client for the flatpak command-line utility.'" >> $HOME/.local/share/applications/flatty.desktop
 echo "Exec=$HOME/flatty/bin/flatty" >> $HOME/.local/share/applications/flatty.desktop
