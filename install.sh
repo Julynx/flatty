8615e2054b358a5f468cf8cf6f4f4519faaede6e
Julynx <64993676+Julynx@users.noreply.github.com>
2022-05-18 23:25:29 +0200

    Installs to .local/bin (icon to .local/share/icons)

--- install.sh
+++ install.sh
@@ -2,27 +2,37 @@
 
 ##
 # @file    install.sh
-# @date    22/03/2022
+# @date    18/05/2022
 # @author  Julio Cabria
 ##
 
-echo "[install.sh] Installing Flatty in $HOME/flatty."
+echo "[install.sh] Installing Flatty in: $HOME/.local/bin/flatty"
 
-# Create a folder named "flatty" inside the home folder
-mkdir -p $HOME/flatty
+# Create the folders if they dont exist
+mkdir $HOME/.local > /dev/null 2>&1
+mkdir $HOME/.local/bin > /dev/null 2>&1
+mkdir $HOME/.local/share > /dev/null 2>&1
+mkdir $HOME/.local/share/icons > /dev/null 2>&1
+mkdir $HOME/.local/share/icons/flatty > /dev/null 2>&1
 
-# Copy the "bin" and "icons" folders inside the "flatty" folder
-cp -r bin $HOME/flatty/
-cp -r icons $HOME/flatty/
+# Install the binary and icon into the folders
+cp bin/flatty $HOME/.local/bin/flatty
+cp icons/flatty.png $HOME/.local/share/icons/flatty/flatty.png
 
 # Generate a .desktop file
 echo "[Desktop Entry]" > $HOME/.local/share/applications/flatty.desktop
 echo "Name=Flatty" >> $HOME/.local/share/applications/flatty.desktop
 echo "Comment='Simple flatpak GUI client for the flatpak command-line utility.'" >> $HOME/.local/share/applications/flatty.desktop
-echo "Exec=$HOME/flatty/bin/flatty" >> $HOME/.local/share/applications/flatty.desktop
+echo "Exec=$HOME/.local/bin/flatty" >> $HOME/.local/share/applications/flatty.desktop
 echo "Terminal=false" >> $HOME/.local/share/applications/flatty.desktop
 echo "Type=Application" >> $HOME/.local/share/applications/flatty.desktop
-echo "Icon=$HOME/flatty/icons/flatty.png" >> $HOME/.local/share/applications/flatty.desktop
+echo "Icon=$HOME/.local/share/icons/flatty/flatty.png" >> $HOME/.local/share/applications/flatty.desktop
+
 update-desktop-database ~/.local/share/applications
+
+echo "[install.sh] Cleaning up..."
+
+rm -r bin
+rm -r icons
 
 echo "[install.sh] Installation finished."
--- uninstall.sh
+++ uninstall.sh
@@ -2,17 +2,19 @@
 
 ##
 # @file    uninstall.sh
-# @date    22/03/2022
+# @date    18/05/2022
 # @author  Julio Cabria
 ##
 
 echo "[uninstall.sh] Removing Flatty..."
 
-# removed the folder named "flatty" inside the home folder
-rm -r $HOME/flatty
+# removed the binary and the icon
+rm $HOME/.local/bin/flatty
+rm $HOME/.local/share/icons/flatty/flatty.png
 
 # Remove the "flatty.desktop" file from ".local/share/applications"
 rm $HOME/.local/share/applications/flatty.desktop
+
 update-desktop-database ~/.local/share/applications
 
 echo "[uninstall.sh] Flatty uninstalled successfully..."
